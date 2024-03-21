0577794e32f604e63f8c61d20871806081780a35
Dirk Luedtke <daimonjiclimber-codes@yahoo.com>
2024-03-21 15:17:22 +0100

    add integer division tests

---
+++ pynrs_python/integer_division.py
@@ -0,0 +1,5 @@
+assert  2 // 3 ==  0 and  2 % 3 == 2
+assert  1 // 3 ==  0 and  1 % 3 == 1
+assert  0 // 3 ==  0 and  0 % 3 == 0
+assert -1 // 3 == -1 and -1 % 3 == 2
+assert -2 // 3 == -1 and -2 % 3 == 1
